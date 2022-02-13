810f7329be9d5140dd4d27a628fd24f6a7bbec55
Gabriel Moreno <gantoreno@gmail.com>
2022-02-13 17:10:00 -0400

    Make container white

--- src/shared/navigation/AppNavigator.tsx
+++ src/shared/navigation/AppNavigator.tsx
@@ -2,18 +2,19 @@ import React, { ReactElement } from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { MainStack } from './MainStackNavigator';
+import { theme } from '../../constants/theme';
 
 export const AppNavigator = (): ReactElement => {
-  const theme = {
+  const navigationTheme = {
     dark: false,
     colors: {
       ...DefaultTheme.colors,
-      background: 'transparent',
+      background: theme.white,
     },
   };
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={navigationTheme}>
       <MainStack />
     </NavigationContainer>
   );
